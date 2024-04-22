fa9216e967891cbb2634f2db723b74ca00687da0
PATEL MOHIT <mohitptl183@gmail.com>
2024-04-22 18:35:31 +0530

    remove Ecom from StoreHome.

--- src/componetnts/store/StoreHome.jsx
+++ src/componetnts/store/StoreHome.jsx
@@ -22,40 +22,6 @@ function StoreHome() {
   };
   
 // add E-commerce tracking code
-useEffect(() => {
-  // Track the view_item event when the component mounts
-  window.dataLayer.push({
-    event: "view_item",
-    ecommerce: {
-      currency: "USD",
-      value: 30.03,
-      items: [
-        {
-          item_id: "1",
-          item_name: "Girls karate uniform",
-          affiliation: "Google Merchandise Store",
-          coupon: "SUMMER_FUN",
-          discount: 2.22,
-          index: 0,
-          item_brand: "Google",
-          item_category: "Apparel",
-          item_category2: "Adult",
-          item_category3: "Shirts",
-          item_category4: "Crew",
-          item_category5: "Short sleeve",
-          item_list_id: "related_products",
-          item_list_name: "Related Products",
-          item_variant: "green",
-          location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
-          price: 199,
-          quantity: 3
-        }
-      ]
-    }
-  });
-}, []);
-
-
   return (
     <>
       <div>
