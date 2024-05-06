85e0476a1d73544dde15c321a5e7517db45f6107
PATEL MOHIT <mohitptl183@gmail.com>
2024-05-06 17:22:33 +0530

    6 may

--- src/componetnts/OurBranch/BranchHome.jsx
+++ src/componetnts/OurBranch/BranchHome.jsx
@@ -18,23 +18,7 @@ function BranchHome() {
   };
   return (
     <>
-          {/* GTM Script in the head */}
-          <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MHHRSMGQ');
-          `,
-        }}
-      />
-      {/* End GTM Script */}
+          
           <div>
         <StateList
          states={BranchData} onSelectState={handleStateClick} />
